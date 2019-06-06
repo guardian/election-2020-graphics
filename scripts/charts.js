d503b7bdee21230c75216b39ebb1653665cedece
sammorrisdesign <mail@sammorrisdesign.co.uk>
2019-06-06 16:55:22 -0400

    populate state map

--- scripts/charts.js
+++ scripts/charts.js
@@ -1,18 +1,43 @@
 var fs = require('fs-extra');
-var D3Node = require('d3-node');
+var d3 = require('d3');
+var jsdom = require('jsdom').JSDOM;
 
 module.exports = {
     compile: function() {
-        this.createChart();
+        fs.mkdirsSync('./.build');
+        this.createStateMap();
     },
 
-    createChart: function() {
-        var d3n = new D3Node();
-        var d3 = d3n.d3;
+    createStateMap: function() {
+        var data = JSON.parse(fs.readFileSync('./assets/2016-election.json', 'utf8'));
+        var valueArray = [];
 
-        // create the chart here
+        data.forEach(function(state, i) {
+            data[i].winner = state.Trump > state.Clinton ? 'Trump' : 'Clinton';
+            data[i].difference = ((parseInt(state.Trump) - parseInt(state.Clinton)) / parseInt(state.Trump)) * 100;
+            valueArray.push(state.difference);
+        });
+
+        var html = new jsdom('<!DOCTYPE html><html><body></body></html>');
+        var preRenderedHTML = fs.readFileSync('./assets/state-map.html', 'utf8')
+
+        var body = d3.select(html.window.document).select('body');
+        var container = body.append('div').attr('class', 'container').html(preRenderedHTML);
+
+        var minVal = d3.min(valueArray);
+        var maxVal = d3.max(valueArray);
+        var ramp = d3.scaleQuantize()
+            .domain([-5, 5])
+            .clamp(true)
+            .ticks(2)
+            .range(['#2e4ea2', '#e11f26']);
+
+        data.forEach(function(state) {
+            container.select('#' + state.State)
+                .attr('fill', ramp(state.difference));
+        })
 
         fs.mkdirsSync('./.build');
-        fs.writeFileSync('./.build/master.html', 'cool');
+        fs.writeFileSync('./.build/master.html', html.window.document.documentElement.outerHTML);
     }
 }
